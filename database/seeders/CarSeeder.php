9bf4d21030a3194e3ed858e2f1bd051010b69962
unknown <wajdtitos@gmail.com>
2026-06-04 01:28:52 +0300

    fix image paths

--- database/seeders/CarSeeder.php
+++ database/seeders/CarSeeder.php
@@ -11,6 +11,41 @@ class CarSeeder extends Seeder
     {
         Car::factory()
             ->count(10)
-            ->create();
+            ->create()
+            ->each(function ($car) {
+
+                $images = $this->carImages();
+
+                shuffle($images);
+
+                $selectedImages = array_slice(
+                    $images,
+                    0,
+                    rand(3, min(5, count($images)))
+                );
+
+                foreach ($selectedImages as $index => $image) {
+                    $car->images()->create([
+                        'path' => $image,
+                        'is_main' => $index === 0,
+                        'alt' => 'Car image',
+                    ]);
+                }
+            });
+    }
+
+    private function carImages(): array
+    {
+        return [
+            'cars/car1.webp',
+            'cars/car2.webp',
+            'cars/car3.webp',
+            'cars/car4.webp',
+            'cars/car5.webp',
+            'cars/car6.webp',
+            'cars/car7.webp',
+            'cars/car8.webp',
+            'cars/car9.webp',
+        ];
     }
 }
